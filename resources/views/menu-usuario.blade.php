bcf94f2293114a66325d805e309bc548bcb1dc92
saulquiroga <pedritbuum@gmail.com>
2020-10-11 22:34:32 -0400

    inicio-login-registro

---
+++ resources/views/menu-usuario.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts/head-login')
+
+
+@section('Cuerpo')
+<!DOCTYPE html>
+<html>
+
+<body>
+  <section id="hero">
+
+  </section>
+ 
+ 
+</body>
+</html>
+@endsection
---
+++ resources/views/salas-creadas.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts/head-login')
+
+
+@section('Cuerpo')
+<!DOCTYPE html>
+<html>
+
+<body>
+  <section id="hero"></section>
+ aqui se encuentran las salas que el usuario creo
+ 
+</body>
+</html>
+@endsection
